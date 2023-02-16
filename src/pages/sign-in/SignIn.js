74ca60519c6275a0a9d9be3cbeb96fc35c675ecc
Quang Huy <85470086+QuangHuyXX@users.noreply.github.com>
2023-02-16 15:08:22 +0700

    Fix Login Admin UI
    Fix css & component

--- src/pages/sign-in/SignIn.js
+++ src/pages/sign-in/SignIn.js
@@ -1,42 +1,55 @@
-import style from "./SignIn.module.scss"
-import classNames from "classnames/bind"
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import style from "./SignIn.module.scss";
+import classNames from "classnames/bind";
+import TextField from "@mui/material/TextField";
 
 const cx = classNames.bind(style);
 
 function SignIn() {
-    return (
-        <div className={cx("main")}>
-            <div className={cx("wrap-login")}>
-                <div className={cx("background-image")}>
-                    <div className={cx("background-image-overlay")}>
-                    </div>
-                </div>
-                <div className={cx("container")}>
-                    <div className={cx("title-login")}>
-                        <div className={cx("title")}>Đăng nhập</div>
-                    </div>
-                    <form className={cx("form-login")}>
-                        <div className={cx("form-group")}>
-                            <label className={cx("label")} htmlFor="User">Username</label>
-                            <TextField className={cx("input")} id="outlined-basic"  variant="outlined" />
-
-                        </div>
-                        <div className={cx("form-group")}>
-                            <label className={cx("label")} htmlFor="Password">Mật khẩu</label>
-                            <TextField className={cx("input")} id="outlined-basic"  variant="outlined" />
-                        </div>
-                        <div className={cx("button")}>
-                            <button className={cx("btn-login")}>Đăng nhập</button>
-                        </div>
-                    </form>
-                </div>
-
-            </div>
-
+  return (
+    <div className={cx("main")}>
+      <div className={cx("wrap-login")}>
+        <div className={cx("background-image")}>
+          <div className={cx("background-image-overlay")}></div>
         </div>
-    );
+        <div className={cx("container")}>
+          <div className={cx("title-login")}>
+            <div className={cx("title")}>Đăng nhập</div>
+          </div>
+          <form className={cx("form-login")}>
+            <div className={cx("form-group")}>
+              <label className={cx("label")} htmlFor="User">
+                Username
+              </label>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                type="text"
+                sx={{  '& .MuiInputBase-root': {
+                    borderRadius: '15px',
+                  }, }}
+              />
+            </div>
+            <div className={cx("form-group")}>
+              <label className={cx("label")} htmlFor="Password">
+                Mật khẩu
+              </label>
+              <TextField
+                id="outlined-password-input"
+                type="password"
+                autoComplete="current-password"
+                sx={{  '& .MuiInputBase-root': {
+                    borderRadius: '15px',
+                  }, }}
+              />
+            </div>
+            <div className={cx("button")}>
+              <button className={cx("btn-login")}>Đăng nhập</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
