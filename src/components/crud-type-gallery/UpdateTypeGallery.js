efc16702b83a0f9ef755dff13686bb25ed14ace0
Quang Huy <85470086+QuangHuyXX@users.noreply.github.com>
2023-05-23 14:33:02 +0700

    Update UpdateTypeGallery.js

--- src/components/crud-type-gallery/UpdateTypeGallery.js
+++ src/components/crud-type-gallery/UpdateTypeGallery.js
@@ -96,7 +96,7 @@ function UpdateTypeGallery(props) {
                         borderRadius: "10px",
                     },
                 }}>
-                <DialogTitle className={cx("dialog-title")} sx={{ fontWeight: "bold" }}>Thêm Loại Ảnhh</DialogTitle>
+                <DialogTitle className={cx("dialog-title")} sx={{ fontWeight: "bold" }}>Cập nhật Loại Ảnh</DialogTitle>
                 <DialogContent>
                     <Box
                         id="type_gallery-form"
